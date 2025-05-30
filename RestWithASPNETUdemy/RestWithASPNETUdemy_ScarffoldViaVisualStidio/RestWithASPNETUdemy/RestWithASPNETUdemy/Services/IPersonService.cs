83c7c31d2392afb53a6b217c7713eb373f91094f
Edson André <andreedson02@gmail.com>
2025-05-29 21:47:11 -0300

    Finalizado primeiro projeto do curso de .NET

---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy_ScarffoldViaVisualStidio/RestWithASPNETUdemy/RestWithASPNETUdemy/Services/IPersonService.cs
@@ -0,0 +1,16 @@
+﻿using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person Update(Person person);
+
+        List<Person> FindAll();
+
+        Person FindByID(long id);
+        void Delete(long id);
+
+    }
+}
